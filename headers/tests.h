524bac750a1cb0f81b9f65f12d92d72d8c0c2816
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-04-19 16:25:16 +0800

    Add tests

---
+++ headers/tests.h
@@ -0,0 +1,16 @@
+#ifndef TESTS_H_
+#define TESTS_H_
+
+#include <stdlib.h>
+#include "radarpredictor.h"
+#include "statepredictor.h"
+#include "../src/Eigen/Dense"
+
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+bool test_compute_augmented_sigma();
+
+
+#endif /* TESTS_H_ */
