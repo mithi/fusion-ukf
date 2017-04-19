2c92bf97b58f4b5e41fb407542c51a9299ec5005
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-04-19 16:25:52 +0800

    Make sigma_z a class variable

--- src/radarpredictor.cpp
+++ src/radarpredictor.cpp
@@ -12,8 +12,8 @@ RadarPredictor::RadarPredictor(const VectorXd W){
 MatrixXd RadarPredictor::compute_sigma_z(const MatrixXd sigma_x){
 
   double px, py, v, yaw, vx, vy, rho, phi, rhodot;
-  MatrixXd sigma_z = MatrixXd(NZ_RADAR, NSIGMA);
-  sigma_z.fill(0.0);
+  MatrixXd sigma = MatrixXd(NZ_RADAR, NSIGMA);
+  sigma.fill(0.0);
 
   for (int c = 0; c < NSIGMA; c++){
 
@@ -29,27 +29,27 @@ MatrixXd RadarPredictor::compute_sigma_z(const MatrixXd sigma_x){
     phi = atan2(py, px);
     rhodot = (px * vx + py * vy) / rho;
 
-    sigma_z(0, c) = rho;
-    sigma_z(1, c) = phi;
-    sigma_z(2, c) = rhodot;
+    sigma(0, c) = rho;
+    sigma(1, c) = phi;
+    sigma(2, c) = rhodot;
   }
 
-  return sigma_z;
+  return sigma;
 }
 
-MatrixXd RadarPredictor::compute_z(const MatrixXd sigma_z){
+MatrixXd RadarPredictor::compute_z(const MatrixXd sigma){
 
   VectorXd z = VectorXd(NZ_RADAR);
   z.fill(0.0);
 
   for(int c = 0; c < NSIGMA; c++){
-    z += this->w(c) * sigma_z.col(c);
+    z += this->w(c) * sigma.col(c);
   }
 
   return z;
 }
 
-MatrixXd RadarPredictor::compute_S(const MatrixXd sigma_z, const MatrixXd z){
+MatrixXd RadarPredictor::compute_S(const MatrixXd sigma, const MatrixXd z){
 
   VectorXd dz = VectorXd(NZ_RADAR);
   MatrixXd S = MatrixXd(NZ_RADAR, NZ_RADAR);
@@ -57,7 +57,7 @@ MatrixXd RadarPredictor::compute_S(const MatrixXd sigma_z, const MatrixXd z){
 
   for (int c = 0; c < NSIGMA; c++){
 
-    dz = sigma_z.col(c) - z;
+    dz = sigma.col(c) - z;
     dz(1) = normalize(dz(1));
 
     S += this->w(c) * dz * dz.transpose();
@@ -70,11 +70,11 @@ MatrixXd RadarPredictor::compute_S(const MatrixXd sigma_z, const MatrixXd z){
 void RadarPredictor::process(const MatrixXd sigma_x){
 
   // transform predicted sigma_x into measurement space
-  MatrixXd sigma_z = this->compute_sigma_z(sigma_x);
+  this->sigma_z = this->compute_sigma_z(sigma_x);
   // get the mean predicted measurement vector z
-  this->z = this->compute_z(sigma_z);
+  this->z = this->compute_z(this->sigma_z);
   // get the measurement covariance matrix S
-  this->S = this->compute_S(sigma_z, this->z);
+  this->S = this->compute_S(this->sigma_z, this->z);
 }
 
 VectorXd RadarPredictor::getz() const {
@@ -83,4 +83,8 @@ VectorXd RadarPredictor::getz() const {
 
 MatrixXd RadarPredictor::getS() const {
   return this->S;
+}
+
+MatrixXd RadarPredictor::get_sigma() const {
+  return this->sigma_z;
 }
