a082c8e68c834b268900327890f33f769da2b6ff
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-04-19 17:47:54 +0800

    Add 4th test

--- headers/tests.h
+++ headers/tests.h
@@ -13,6 +13,7 @@ using Eigen::VectorXd;
 bool test_compute_augmented_sigma();
 bool test_predict_sigma_x();
 bool test_predict_x_P();
+bool radar_predictor_process_test();
 
 
 #endif /* TESTS_H_ */
--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,11 @@ using Eigen::VectorXd;
 int main(){
   cout << "ALL TESTS SHOULD RETURN 1" << endl;
   cout << "- StatePredictor Tests" << endl;
-  cout << "--- compute_augmented_sigma: "<< test_compute_augmented_sigma() << endl;
-  cout << "--- predict_sigma: "<< test_predict_sigma_x() << endl;
-  cout << "--- predict_x_P: "<< test_predict_x_P() << endl;
+  cout << "--- compute_augmented_sigma(): "<< test_compute_augmented_sigma() << endl;
+  cout << "--- predict_sigma(): "<< test_predict_sigma_x() << endl;
+  cout << "--- predict_x_P(): "<< test_predict_x_P() << endl;
+  cout << "- RadarPredictor Tests" << endl;
+  cout << "--- radar_predictor_process_test(): "<< radar_predictor_process_test() << endl;
+
   return 0;
 }
