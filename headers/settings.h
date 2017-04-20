11d6b30a120779a5c31ab7a54853bac356c11ead
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-04-20 23:40:20 +0800

    Tweak parameters

--- headers/settings.h
+++ headers/settings.h
@@ -12,15 +12,15 @@ const int NAUGMENTED = NX + 2; // number of states plus two noise values
 const int NSIGMA = NAUGMENTED * 2 + 1; // number of sigma points
 
 //process noise standard deviations
-const double STD_SPEED_NOISE = 0.2; // longitudinal acceleration in m/s^2
-const double STD_YAWRATE_NOISE = 0.2; // process noise standard deviation yaw acceleration in rad/s^2
+const double STD_SPEED_NOISE = 1.5; // longitudinal acceleration in m/s^2
+const double STD_YAWRATE_NOISE = 0.57; // process noise standard deviation yaw acceleration in rad/s^2
 const double VAR_SPEED_NOISE = STD_SPEED_NOISE * STD_SPEED_NOISE;
 const double VAR_YAWRATE_NOISE = STD_YAWRATE_NOISE * STD_YAWRATE_NOISE;
 
 //RADAR measurements noise standard deviations
 const double STD_RHO = 0.3; // meters
-const double STD_PHI = 0.0175; // radians
-const double STD_RHODOT = 0.1; // meters / second
+const double STD_PHI = 0.03; // radians
+const double STD_RHODOT = 0.3; // meters / second
 const double VAR_RHO = STD_RHO * STD_RHO;
 const double VAR_PHI = STD_PHI * STD_PHI;
 const double VAR_RHODOT = STD_RHODOT * STD_RHODOT;
