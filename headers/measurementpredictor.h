c099df6d3e1798dd9980967f9dae3acff900e4eb
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-04-20 10:45:00 +0800

    Add class MeasurementPredictor

---
+++ headers/measurementpredictor.h
@@ -0,0 +1,44 @@
+#ifndef MEASUREMENTPREDICTOR_H_
+#define MEASUREMENTPREDICTOR_H_
+
+#include "../src/Eigen/Dense"
+#include "tools.h"
+#include "settings.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+/******************************
+ MeasurementPredictor is a class responsible for
+ calculating the predicted measurement vector z, measurement covariance matrix S
+ and sigma point matrix transformed to measurement space sigma_z
+ based on a given predicted sigma points matrix sigma_x
+
+ After calling the process() please call getS(), getz() and get_sigma()
+ to get the corresponding calculated values
+*******************************/
+
+class MeasurementPredictor{
+
+  private:
+    int nz;
+    MatrixXd R;
+    VectorXd z;
+    MatrixXd S;
+    MatrixXd sigma_z;
+
+  public:
+    MeasurementPredictor();
+    void process(const MatrixXd sigma_x, const DataPointType sensor_type);
+    MatrixXd get_sigma() const;
+    MatrixXd getS() const;
+    VectorXd getz() const;
+
+    // PRIVATE FUNCTIONS
+    void initialize(const DataPointType sensor_type);
+    MatrixXd compute_sigma_z(const MatrixXd sigma_x);
+    MatrixXd compute_z(const MatrixXd sigma_z);
+    MatrixXd compute_S(const MatrixXd sigma_z, const MatrixXd predicted_z);
+};
+
+#endif //MEASUREMENTPREDICTOR_H_
---
+++ src/measurementpredictor.cpp
@@ -0,0 +1,119 @@
+#include "measurementpredictor.h"
+
+MeasurementPredictor::MeasurementPredictor(){}
+
+void MeasurementPredictor::initialize(const DataPointType sensor_type){
+
+  if(sensor_type == DataPointType::RADAR){
+
+    this->nz = NZ_RADAR;
+    this->R = MatrixXd(this->nz, this->nz);
+    this->R << VAR_RHO, 0, 0,
+               0, VAR_PHI, 0,
+               0, 0, VAR_RHODOT;
+
+  } else { // LIDAR
+
+    this->nz = NZ_LIDAR;
+    this->R = MatrixXd(this->nz, this->nz);
+    this->R << VAR_PX, 0,
+               0, VAR_PY;
+  }
+}
+
+MatrixXd MeasurementPredictor::compute_sigma_z(const MatrixXd sigma_x){
+
+  double px, py, v, yaw, vx, vy, rho, phi, rhodot;
+
+  MatrixXd sigma = MatrixXd(this->nz, NSIGMA);
+  sigma.fill(0.0);
+
+  for (int c = 0; c < NSIGMA; c++){
+
+    if (this->nz == NZ_RADAR){
+
+      px = sigma_x(0, c);
+      py = sigma_x(1, c);
+      v = sigma_x(2, c);
+      yaw = sigma_x(3, c);
+
+      vx = cos(yaw) * v;
+      vy = sin(yaw) * v;
+
+      rho = sqrt(px * px + py * py);
+      phi = atan2(py, px);
+      rhodot = (px * vx + py * vy) / rho;
+
+      sigma(0, c) = rho;
+      sigma(1, c) = phi;
+      sigma(2, c) = rhodot;
+
+    } else { //LIDAR
+
+      px = sigma_x(0, c);
+      py = sigma_x(1, c);
+
+      sigma(0, c) = px;
+      sigma(1, c) = py;
+    }
+  }
+
+  return sigma;
+}
+
+MatrixXd MeasurementPredictor::compute_z(const MatrixXd sigma){
+
+  VectorXd z = VectorXd(this->nz);
+  z.fill(0.0);
+
+  for(int c = 0; c < NSIGMA; c++){
+    z += WEIGHTS[c] * sigma.col(c);
+  }
+
+  return z;
+}
+
+MatrixXd MeasurementPredictor::compute_S(const MatrixXd sigma, const MatrixXd z){
+
+  VectorXd dz;
+  MatrixXd S = MatrixXd(this->nz, this->nz);
+  S.fill(0.0);
+
+  for (int c = 0; c < NSIGMA; c++){
+
+    dz = sigma.col(c) - z;
+    dz(1) = normalize(dz(1));
+
+    S += WEIGHTS[c] * dz * dz.transpose();
+  }
+
+  S += this->R;
+  return S;
+}
+
+void MeasurementPredictor::process(const MatrixXd sigma_x, const DataPointType sensor_type){
+
+  // let the processor know whether it's processing a LIDAR or RADAR measurement
+  this->initialize(sensor_type);
+
+  // transform predicted sigma_x into measurement space
+  this->sigma_z = this->compute_sigma_z(sigma_x);
+
+  // get the mean predicted measurement vector z
+  this->z = this->compute_z(this->sigma_z);
+  
+  // get the measurement covariance matrix S
+  this->S = this->compute_S(this->sigma_z, this->z);
+}
+
+VectorXd MeasurementPredictor::getz() const {
+  return this->z;
+}
+
+MatrixXd MeasurementPredictor::getS() const {
+  return this->S;
+}
+
+MatrixXd MeasurementPredictor::get_sigma() const {
+  return this->sigma_z;
+}
