b6d4dbe7c4d766e5861d1fbf71b5fd6caeb96a5f
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-04-20 23:41:09 +0800

    Add DataPoint class

---
+++ headers/datapoint.h
@@ -0,0 +1,33 @@
+#ifndef DATAPOINT_H_
+#define DATAPOINT_H_
+
+#include <stdlib.h>
+#include <iostream>
+#include "../src/Eigen/Dense"
+#include "settings.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using namespace std;
+
+class DataPoint{
+
+  private:
+    long long timestamp;
+    bool initialized;
+    DataPointType data_type;
+    VectorXd raw;
+
+  public:
+    DataPoint();
+    DataPoint(const long long timestamp, const DataPointType data_type, const VectorXd raw);
+    void set(long timestamp, const DataPointType data_type, const VectorXd raw);
+    VectorXd get() const;
+    VectorXd get_state() const;
+    VectorXd get_vec() const;
+    DataPointType get_type() const;
+    long long get_timestamp() const;
+    void print() const;
+};
+
+#endif /* DATAPOINT_H_*/
---
+++ src/datapoint.cpp
@@ -0,0 +1,120 @@
+#include "datapoint.h"
+
+DataPoint::DataPoint(){
+  this->initialized = false;
+}
+
+DataPoint:: DataPoint(const long long timestamp, const DataPointType data_type, const VectorXd raw){
+  this->set(timestamp, data_type, raw);
+}
+
+void DataPoint::set(const long timestamp, const DataPointType data_type, const VectorXd raw){
+  this->timestamp = timestamp;
+  this->data_type = data_type;
+  this->raw = raw;
+  this->initialized = true;
+}
+
+VectorXd DataPoint::get() const{
+  return this->raw;
+}
+
+VectorXd DataPoint::get_state() const{
+
+  VectorXd state(NX);
+
+  if (this->data_type == DataPointType::LIDAR){
+
+    double px = this->raw(0);
+    double py = this->raw(1);
+    state << px, py, 0.0, 0.0, 0.0;
+
+  } else if (this->data_type == DataPointType::RADAR){
+
+    double rho = this->raw(0);
+    double phi = this->raw(1);
+
+    double px = rho * cos(phi);
+    double py = rho * sin(phi);
+    state << px, py, 0.0, 0.0, 0.0;
+
+  } else if (this->data_type == DataPointType::STATE){
+    state = this->raw;
+
+  } else if (this->data_type == DataPointType::TRUTH){
+
+    double px = this->raw(0);
+    double py = this->raw(1);
+    double vx = this->raw(2);
+    double vy = this->raw(3);
+
+    double v = sqrt(vx * vx + vy * vy);
+    double yaw = atan2(vy, vx);
+    state << px, py, v, yaw, 0.0;
+  }
+
+  return state;
+}
+
+VectorXd DataPoint::get_vec() const{
+
+  VectorXd vec(NX - 1);
+
+  if (this->data_type == DataPointType::LIDAR){
+
+    double px = this->raw(0);
+    double py = this->raw(1);
+    vec << px, py, 0.0, 0.0;
+
+  } else if (this->data_type == DataPointType::RADAR){
+
+    double rho = this->raw(0);
+    double phi = this->raw(1);
+    double px = rho * cos(phi);
+    double py = rho * sin(phi);
+
+    vec << px, py, 0.0, 0.0;
+
+  } else if (this->data_type == DataPointType::STATE){
+
+    double px = this->raw(0);
+    double py = this->raw(1);
+    double v = this->raw(3);
+    double yaw = this->raw(4);
+
+    double vx = v * cos(yaw);
+    double vy = v * sin(yaw);
+
+    vec << px, py, vx, vy;
+
+  } else if (this->data_type == DataPointType::TRUTH){
+
+    vec = this->raw;
+  }
+
+  return vec;
+}
+
+
+long long DataPoint::get_timestamp() const{
+  return this->timestamp;
+}
+
+DataPointType DataPoint::get_type() const{
+  return this->data_type;
+}
+
+void DataPoint::print() const{
+
+  if (this->initialized){
+
+    cout << "Timestamp: " << this->timestamp << endl;
+    cout << "Sensor ID: " << static_cast<int>(this->data_type) << " (LIDAR = 0 | RADAR = 1 | STATE = 2) " << endl;
+    cout << "Raw Data: " << endl;
+    cout << this->raw << endl;
+
+  } else {
+
+    cout << "DataPoint is not initialized." << endl;
+  }
+}
