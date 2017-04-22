a9704f66115bdce655c00759b0a034e22ef1becf
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-04-22 15:09:44 +0800

    Prettify formatting

--- src/usagecheck.cpp
+++ src/usagecheck.cpp
@@ -40,44 +40,44 @@ void check_files(ifstream& in_file, string& in_name, ofstream& out_file, string&
 void print_EKF_data(const VectorXd &RMSE, const vector<VectorXd> &estimations,
    const vector<VectorXd> &ground_truths, const vector<DataPoint> &all_sensor_data){
 
-  cout << "--------------------------------------------------------------------" << endl;
+  cout << "----------------------------------------------------------------------------------------------------------" << endl;
   cout << setw(15) << left << "RMSE:" << " | " << RMSE(0) << " | " << RMSE(1) << " | " << RMSE(2) << " | " << RMSE(3) << " | " << endl;
-  cout << "--------------------------------------------------------------------" << endl;
+  cout << "----------------------------------------------------------------------------------------------------------" << endl;
 
   for (int k = 0; k < all_sensor_data.size(); k++){
 
-    cout << "--------------------------------------------------------------------" << endl;
+    cout << "----------------------------------------------------------------------------------------------------------" << endl;
     cout  << "# " << k + 1 << " : " << all_sensor_data[k].get_timestamp() << endl;
-    cout << "--------------------------------------------------------------------" << endl;
+    cout << "----------------------------------------------------------------------------------------------------------" << endl;
 
     if (all_sensor_data[k].get_type() == DataPointType::LIDAR){
 
       VectorXd lidar_v = all_sensor_data[k].get();
       cout << setw(15) << left << "LIDAR:" << " | ";
-      cout << setw(10) << left << lidar_v(0) << " | ";
-      cout << setw(10) << left << lidar_v(1) << " | " << endl;
+      cout << setw(15) << left << lidar_v(0) << " | ";
+      cout << setw(15) << left << lidar_v(1) << " | " << endl;
 
     } else if (all_sensor_data[k].get_type() == DataPointType::RADAR){
 
       VectorXd radar_v = all_sensor_data[k].get();
       cout << setw(15) << left << "RADAR:" << " | ";
-      cout << setw(10) << left << radar_v(0) << " | ";
-      cout << setw(10) << left << radar_v(1) << " | ";
-      cout << setw(10) << left << radar_v(2) << " | " << endl;
+      cout << setw(15) << left << radar_v(0) << " | ";
+      cout << setw(15) << left << radar_v(1) << " | ";
+      cout << setw(15) << left << radar_v(2) << " | " << endl;
     }
 
     cout << setw(15) << left << "PREDICTION:" << " | ";
-    cout << setw(10) << left << estimations[k](0) << " | ";
-    cout << setw(10) << left << estimations[k](1) << " | ";
-    cout << setw(10) << left << estimations[k](2) << " | ";
-    cout << setw(10) << left << estimations[k](3) << " | ";
-    cout << setw(10) << left << estimations[k](4) << " | " << endl;
+    cout << setw(15) << left << estimations[k](0) << " | ";
+    cout << setw(15) << left << estimations[k](1) << " | ";
+    cout << setw(15) << left << estimations[k](2) << " | ";
+    cout << setw(15) << left << estimations[k](3) << " | ";
+    cout << setw(15) << left << estimations[k](4) << " | " << endl;
 
     cout << setw(15) << left << "TRUTH:" << " | ";
-    cout << setw(10) << left << ground_truths[k](0) << " | ";
-    cout << setw(10) << left << ground_truths[k](1) << " | ";
-    cout << setw(10) << left << ground_truths[k](2) << " | ";
-    cout << setw(10) << left << ground_truths[k](3) << " | ";
-    cout << setw(10) << left << ground_truths[k](4) << " | " << endl;
+    cout << setw(15) << left << ground_truths[k](0) << " | ";
+    cout << setw(15) << left << ground_truths[k](1) << " | ";
+    cout << setw(15) << left << ground_truths[k](2) << " | ";
+    cout << setw(15) << left << ground_truths[k](3) << " | ";
+    cout << setw(15) << left << ground_truths[k](4) << " | " << endl;
   }
 }
