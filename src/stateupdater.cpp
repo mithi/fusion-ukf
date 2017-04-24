ace58c320e2849a381551c76d5b5450d38fab94b
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-04-24 12:25:43 +0800

    Remove unneeded console prints

--- src/stateupdater.cpp
+++ src/stateupdater.cpp
@@ -18,7 +18,7 @@ MatrixXd StateUpdater::compute_Tc(const VectorXd predicted_x,
     dx(3) = normalize(dx(3));
 
     dz = sigma_z.col(c) - predicted_z;
-    if (NZ == NZ_RADAR) dz(1) = normalize(dz(1)); // phi in radians
+    if (NZ == NZ_RADAR) dz(1) = normalize(dz(1));
 
     Tc += WEIGHTS[c] * dx * dz.transpose();
   }
@@ -35,20 +35,9 @@ void StateUpdater::update(const VectorXd z, const MatrixXd S, const MatrixXd Tc,
   VectorXd dz = z - predicted_z;
   if (dz.size() == NZ_RADAR) dz(1) = normalize(dz(1)); // yaw/phi in radians
 
-  cout << "Tc" << Tc << endl;
-  cout << "S" << S << endl;
-  cout << "Si" << Si << endl;
-  cout << "K" << K << endl;
-  cout << "dz" << dz << endl;
-  cout << "x" << predicted_x << endl;
-  cout << "P" << predicted_P << endl;
-
   this->x = predicted_x + K * dz;
   this->P = predicted_P - K * S * K.transpose();
   this->nis = dz.transpose() * Si * dz;
-
-
-
 }
 
 void StateUpdater::process(const VectorXd predicted_x, const VectorXd predicted_z,
